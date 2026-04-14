87057c3a1a023dfede0ff8be923a5d220a806591
Inupama <inupamacaldera@gmail.com>
2026-04-14 15:47:25 +0530

    Added role repository

--- backend/src/main/java/com/smartcampus/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/smartcampus/backend/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package com.smartcampus.backend.repository;
 
-public interface RoleRepository {
+import com.smartcampus.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+    boolean existsByName(String name);
 }
